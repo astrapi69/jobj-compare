aa48d3198f1ae121e4aa679146928de20fddba1a
Asterios Raptis <asteri.raptis@gmail.com>
2024-06-21 16:24:09 +0200

    fixed warnings

--- src/main/java/io/github/astrapi69/compare/object/CompareObjectExtensions.java
+++ src/main/java/io/github/astrapi69/compare/object/CompareObjectExtensions.java
@@ -29,6 +29,7 @@ import java.util.Set;
 
 import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.beanutils.BeanUtils;
+
 import io.github.astrapi69.comparator.ComparatorExtensions;
 
 /**
